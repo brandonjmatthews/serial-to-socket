aa4e8de412eb2a8494a01960f4a52980d48b0406
Brandon Matthews <brandonjmatthews@outlook.com>
2023-01-26 18:45:43 +1030

    Split recieve into sepeerate thread

--- start.py
+++ start.py
@@ -6,6 +6,7 @@ import time
 import select
 
 
+RESTART_SLEEP = 1
 PING_DELAY = 100
 HOST = '' # Standard loopback interface address (localhost)
 PORT = 54321  # Port to listen on (non-privileged ports are > 1023)
@@ -18,14 +19,15 @@ class SerialNetworkLink():
         self.serial_port_name = port.device
         self.socket_port_number = socket_port_number
         self.last_ping = time.time()
+        self.is_connected = False
+        self.restarting = False
 
     def Connect(self):
         self.serial_connection = Serial(self.serial_port_name,  baudrate=115200, write_timeout=10)
         print(f'[{self.link_name}] Serial port:{self.serial_port_name} linked to Socket port:{self.socket_port_number}')
         
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+        self.socket = socket.socket(socket.AF_INET)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        print(self.socket_port_number)
         self.socket.bind((HOST, self.socket_port_number))
         #self.socket = socket.create_server((HOST, self.socket_port_number))
         self.connect_thread = threading.Thread(target=self.AwaitConnection, args=())
@@ -37,14 +39,16 @@ class SerialNetworkLink():
         print(f'[{self.link_name}] Awaiting socket connection...')   
 
         self.connected, self.address = self.socket.accept()
-
-        self.link_thread = threading.Thread(target=self.Transmit, args=())
-        self.link_thread.start()
+        self.is_connected = True
+        self.transmit_thread = threading.Thread(target=self.Transmit, args=())
+        self.transmit_thread.start()
+        self.receive_thread = threading.Thread(target=self.Receive, args=())
+        self.receive_thread.start()
 
         print(f'[{self.link_name}] Connected!')
 
     def Transmit(self):
-        while self.serial_connection.is_open:
+        while self.is_connected:
             if time.time() - self.last_ping > PING_DELAY: 
                print(f'[{self.link_name}] Alive!')
                self.last_ping = time.time()
@@ -56,29 +60,32 @@ class SerialNetworkLink():
                 except:
                     self.Reopen()
                     break
-            try:
-                r,_,_ = select.select([self.socket],[],[],0.01)
 
-                if r:
-                    incoming = self.connected.recv(1024)
-                    if len(incoming) > 0:
-                        #print(f'Recieved: {incoming}')
-                        self.serial_connection.write(incoming)      
+
+    def Receive(self):
+        while self.is_connected:
+            try:
+                incoming = self.connected.recv(1024)
+                if len(incoming) > 0:
+                    print(f'Recieved: {incoming}')
+                    self.serial_connection.write(incoming)  
             except:
                 self.Reopen()
                 break
 
-
     def Close(self):
+        self.is_connected = False
         self.connected.close()
-        self.socket.close()
-        self.serial_connection.close()
+        #self.socket.close()
+        #self.serial_connection.close()
         print(f'[{self.link_name}] Closed')
 
     def Reopen(self):
+        self.closing = True
         self.Close()
-        time.sleep(5)
+        time.sleep(RESTART_SLEEP)
         self.Connect()
+        self.closing = False
 
 
 def main(args):
